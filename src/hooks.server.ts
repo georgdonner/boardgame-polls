157ad34f34f1e800fcc6b2fdb1885f79ba0c3053
Georg Donner <georgdonner@posteo.de>
2022-10-28 11:10:52 +0200

    Pull db connection out of hooks

--- src/hooks.server.ts
+++
@@ -1,9 +0,0 @@
-import { connect } from "$lib/server/db";
-
-connect()
-  .then(() => {
-    console.log('Connected to database');
-  })
-  .catch((e) => {
-    console.error(e);
-  });
--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -3,13 +3,8 @@ import { MONGO_URL } from '$env/static/private';
 
 const client = new MongoClient(MONGO_URL);
 
-export function connect() {
-  console.log('Connecting to database...');
-  return client.connect();
-}
-
-export default client.db();
-
+export default (await client.connect()).db();
+ 
 export interface Boardgame {
   _id: string;
   name: string;
